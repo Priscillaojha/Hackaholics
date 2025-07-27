ad6dcbc91549a0de03789223674063c22ae1f543
Priscilla <prisila.ojha@thamescollege.edu.np>
2025-07-27 03:23:18 +0000

    ✅ Add responsive login UI with curved layout

---
+++ src/components/login.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./login.css";
+import illustration from "../assets/illustration.png"; 
+
+const Login: React.FC = () => {
+  return (
+    <div className="login-container">
+      <div className="left-side">
+        <img src={illustration} alt="Illustration" />
+      </div>
+      <div className="right-side">
+        <h2>LOG IN</h2>
+        <form className="login-form">
+          <div className="input-group">
+            <i className="fas fa-user"></i>
+            <input type="text" placeholder="Username" />
+          </div>
+          <div className="input-group">
+            <i className="fas fa-lock"></i>
+            <input type="password" placeholder="Password" />
+          </div>
+          <div className="form-options">
+            <label>
+              <input type="checkbox" /> Remember me
+            </label>
+            <a href="#">Forgot Password?</a>
+          </div>
+          <button type="submit">Log In</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
